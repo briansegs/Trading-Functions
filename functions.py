7ebc938face80f6fdb6ab0a1d08cee7b67c7b0f1
brian <briansegs@gmail.com>
2022-10-01 15:31:21 -0400

    feat: Add daily sma

--- functions.py
+++ functions.py
@@ -19,16 +19,44 @@ target = 35
 max_loss = -55
 vol_decimal = .4
 
-# Ask and bid:
+# For dataframe:
+timeframe = '4h'
+limit = 100
+sma = 20
+
+
+# ask_bid()[0] = ask, [1] = bid
+# ask_bid(symbol) if none given, uses defaults
 def ask_bid(symbol=symbol):
     ob = kucoin.fetch_order_book(symbol)
-    #print(ob)
 
     bid = ob['bids'][0][0]
     ask = ob['asks'][0][0]
 
     print(f'This is the ask for {symbol}: {ask}')
 
-    return ask, bid # ask_bid()[0] = ask, [1] = bid
+    return ask, bid
 
-#ask_bid()
+# Dataframe SMA: 6:16
+# Returns: dataframe(df_sma) with sma and trade signal
+# Call: df_sma(symbol, timeframe, limit, sma) # if not passed, uses defaults
+def df_sma(symbol=symbol, timeframe=timeframe, limit=limit, sma=sma):
+    print('starting...')
+    bars = kucoin.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
+
+    df_sma = pd.DataFrame(bars, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+    df_sma['timestamp'] = pd.to_datetime(df_sma['timestamp'], unit='ms')
+
+    #Dataframe SMA
+    df_sma[f'sma{sma}_{timeframe}'] = df_sma.close.rolling(sma).mean()
+
+    bid = ask_bid(symbol)[1]
+    # if bid < the sma then = BEARISH, if bid > sma = BULLISH
+    # if sma > bid = SELL, if  sma < bid = BUY
+    df_sma.loc[df_sma[f'sma{sma}_{timeframe}']>bid, 'signal'] = 'SELL'
+    df_sma.loc[df_sma[f'sma{sma}_{timeframe}']<bid, 'signal'] = 'BUY'
+
+    print(df_sma)
+    return df_sma
+
+#23:30
